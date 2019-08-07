c8689a1228fa3a0186d0eb665a50f1676b98dcb3
Aditya Putra <adityaputra7997@gmail.com>
2019-08-07 17:29:51 +0700

    [ADD] Navbar
    Not responsive yet

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+      crossorigin="anonymous"
+    />
+    <Header></Header>
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+  <header>
+    <a href="/">
+      <img src="@/assets/img/logo.png" alt class="logo" />
+    </a>
+    <nav>
+      <ul class="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+
+        <li>
+          <a href>About</a>
+        </li>
+
+        <li>
+          <a href>Portfolio</a>
+        </li>
+
+        <li>
+          <a href>Contact</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div>
+      <ul class="nav-links">
+        <li>
+          <a href="https://www.instagram.com/adityaputral/" target="_blank"> <i class="fab fa-instagram"></i></a>
+        </li>
+
+        <li>
+          <a href="https://www.linkedin.com/in/aditya-putra-leksono/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+
+        <li>
+          <a href="mailto:adityaputra.official@gmail.com" target="_blank"><i class="far fa-envelope"></i></a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #383c4a;
+  padding: 30px 10%;
+}
+
+.nav-links {
+  list-style: none;
+}
+
+.nav-links li {
+  display: inline-block;
+  padding: 0 20px;
+}
+
+.nav-links li a {
+  transition: all 0.3s ease 0s;
+  font-size: 20px;
+  font-weight: 400;
+}
+
+.nav-links li a:hover {
+  color: #5294e2;
+}
+</style>
